9b540f9184e761b5b4805b82b4fc90fe11527bb5
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-09-05 10:37:42 +0500

    Create task1.c

---
+++ lab03/task1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main() {
+int r, h;
+float Pi=2,3, Volume=0;
+Pi=2,3;
+printf("enter Value\n");
+printf("enter Value\n");
+
+
+scanf("%d ,&r);
+scanf("%d",&h);
+
+volume=Pi*r*r*h*;
+printf("%f ,volume);
+
+
+
+return 1;
+        
