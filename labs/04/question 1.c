017346d0dfcbbb58f051f1568df5fcd7f1c4c00d
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-09-12 23:10:18 +0500

    Add files via upload

---
+++ labs/04/question 1.c
@@ -0,0 +1,25 @@
+/*
+ *programmer:Urooj Baloch
+ *Date:11 september 2023
+ *Description:question 1
+ */
+ #include <stdio.h>
+ 
+int main () {
+            int n=0;
+            int x=50
+            
+            
+             while(n<=x){
+                  printf("*\n");
+                  
+                  int m=0;
+                  while (m<=n){
+                      printf("*");
+                      m=m+1;
+                  }
+                  n=n+1;
+              }
+                           
+}//end main()
+       
