b18a36cfc6f8e31142572f529a402613a63a4949
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-09-12 10:43:13 +0500

    Add files via upload

---
+++ labs/04/question2.c
@@ -0,0 +1,24 @@
+/*
+ *Programmer:Urooj Baloch
+ *Date:11th september 2023
+ *Description:Question 1,Lab 4.
+ */
+
+#include <stdio.h>
+int main () {
+              int n;
+              int a=0;
+              int b=0;
+              printf("enter the value of n:\n");
+              scanf("%d",&n);
+              
+              while(a<=n) {
+                           printf("\n");
+                           a=a+1;
+              while(b<n){
+                         printf("* \n");
+                         b=b+1;
+                               }
+}
+}//end main()
+       
