17ececceac4c9def0fe8a75fc4d0b98b3ea4e7f6
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-09-14 20:13:27 +0500

    Add files via upload

---
+++ lab03/task03 question 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int number,number2;
+    
+    printf("enter a number\n");
+    scanf("%d",&number);
+    printf("enter a number2 \n");
+    scanf("%d",&number2);
+     int a=number*number2;
+     printf("result of multiplication is %d\n",a);
+     int b=number/number2;
+     printf("result of division is %d\n",b);
+     int c=number+number2;
+     printf("result of addition is %d\n",c);
+     int d=number-number2;
+     printf("result of subtration is %d\n",d);
+     return 0;
+     
+}
+    
+
+    
