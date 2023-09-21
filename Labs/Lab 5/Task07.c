883d67d102c68ac462365a3eff6746d779fd2715
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-09-21 20:36:20 +0500

    Update Task07.c

--- Labs/Lab 5/Task07.c
+++ Labs/Lab 5/Task07.c
@@ -7,15 +7,8 @@ int main() {
     float amountafterdiscount;
     printf("Enter the prize of items");
     scanf("%d",&actual_amount);
-    printf("your actual amount is %d\n",actual_amount);
-    if(actual_amount==1999){
-        printf("you got upto 50 percent discount\n");
-        savedamount=(actual_amount*40)/100;
-        printf("Your saved amount  is %f\n",savedamount);
-        amountafterdiscount=actual_amount-savedamount;
-        printf("Your amount after discount is%f\n",amountafterdiscount);
-    }
-    else if(actual_amount>=2000 && actual_amount<=4000){
+    printf("your actual amount is %d\n",actual_amount); 
+    if(actual_amount>=2000 && actual_amount<=4000){
         printf("You got 20 percent discount\n");
         savedamount=(actual_amount*20)/100;
         printf("Your saved amount is %f\n",savedamount);
