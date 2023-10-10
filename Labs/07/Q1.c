40a294ba4679b617c6ef21943f6a384493c95634
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-10-10 09:23:42 +0500

    Add files via upload

---
+++ Labs/07/Q1.c
@@ -0,0 +1,30 @@
+/* Online C Compiler and Editor */
+// Online C compiler to run C program online
+   /* Programmer : Urooj baloch
+   Date :10-10-2023
+   Description :divide two numbers without using division operator
+   */
+  #include <stdio.h>
+  int main(){
+  int divisor;
+  int dividend;
+  int quotient=0;
+  printf("enter the value of dividend\n");
+  scanf("%d",&dividend);
+  printf("enter the divisor\n");
+  scanf("%d",&divisor);
+  if(divisor==0){
+                printf("you can not put zero\n");
+}//end if
+ else{
+      while (dividend>=0){
+      dividend-=divisor;
+     
+    //quotient++;
+      }
+      printf("quotient is %d",quotient);
+}//end else
+
+     return 0;
+
+ }//end main
