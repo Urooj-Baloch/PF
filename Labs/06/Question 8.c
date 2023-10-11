23ba66468b372c4c1748086322e8f5fb437baac8
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-10-11 18:57:18 +0500

    Add files via upload

---
+++ Labs/06/Question 8.c
@@ -0,0 +1,22 @@
+/* Programmer:Urooj Baloch
+Date:3-10-2023
+Description:reverse of array
+#include <stdio.h>
+*/
+#include <stdio.h>
+int main() {
+    int n;
+    int i;
+    
+    int arrays[]={n};
+    printf("enter the value of n\n");
+    scanf("\n %d",&n);
+    
+    for(int i=n;i>=1;i--){
+        printf("%d\n",i);
+        n--;
+    }//end for
+    
+    
+    return 0;
+}
