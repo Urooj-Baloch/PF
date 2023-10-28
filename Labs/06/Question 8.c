f7e469f87be24af9cbb68a95328c74de796a2344
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-10-28 10:35:15 +0500

    Update Question 8.c

--- Labs/06/Question 8.c
+++ Labs/06/Question 8.c
@@ -1,22 +1,22 @@
 /* Programmer:Urooj Baloch
 Date:3-10-2023
-Description:reverse of array
+Description:reverse of elements of array taken as input from user
 #include <stdio.h>
 */
 #include <stdio.h>
+
 int main() {
     int n;
-    int i;
-    
-    int arrays[]={n};
-    printf("enter the value of n\n");
+   
+    printf("enter the size of array \n");//take input of size
     scanf("\n %d",&n);
-    
-    for(int i=n;i>=1;i--){
-        printf("%d\n",i);
-        n--;
+    int arr[n];
+    for(int i=0;i<=n-1;i++){
+        printf("enter the element %d \n",i+1);//elements from user
+        scanf("%d",&arr[i]);
     }//end for
-    
-    
+    for(int i=n-1;i>=0;i--){//reverse of those elements
+        printf("%d ",arr[i]);
+    }//end for
     return 0;
-}
+}//end main
