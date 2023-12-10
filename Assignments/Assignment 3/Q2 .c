134d473dafe548e1c4a3f5e86c612b7e6a56dd30
Urooj-Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2023-12-10 12:35:56 +0500

    Update Q2 .c

--- Assignments/Assignment 3/Q2 .c
+++ Assignments/Assignment 3/Q2 .c
@@ -1,111 +1,134 @@
-/*
-programer: Urooj baloch
-Date :6 dec 2023
-description : Q2 
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
+#include <string.h>
 
-#define MAX_EMPLOYEES 5
-#define MAX_DEPARTMENTS 4
-#define MAX_NAME_LENGTH 20
+struct Dept {
+    char Name[20];
+    char Role[20];
+    int Communication;
+    int Teamwork;
+    int Creativity;
+};
 
-typedef struct {
-    char name[MAX_NAME_LENGTH];
-    char role[MAX_NAME_LENGTH];
-    int communication;
-    int teamwork;
-    int creativity;
-} Employee;
+char* NameRand(char Names[20][20], int Name_Used[20]);
+char* RoleRand(char Roles[5][20], int Role_Used[5]);
+void InitializeDept(struct Dept dept[], char Names[20][20], int Name_Used[20]);
+void PrintDept(struct Dept dept[]);
+int DeptSum(struct Dept dept[]);
 
-typedef struct {
-    char name[MAX_NAME_LENGTH];
-    Employee employees[MAX_EMPLOYEES];
-} Department;
+int main() {
+     int i;
+    printf("*******UROOJ BALOCH (23K-0071)*********\n");
+    srand(time(NULL));
 
-void initializeNames(char names[][MAX_NAME_LENGTH], int numNames) {
-    const char *poolOfNames[] = {"John", "Jane", "Bob", "Alice", "David", "Emma", "Michael", "Olivia", "James", "Sophia",
-                                 "William", "Isabella", "Daniel", "Emily", "Matthew", "Abigail", "Christopher", "Mia", "Andrew", "Ella"};
+    char NamePool[20][20] = {"urooj", "Zani", "Johar", "Farhana",
+                              "Bushra", "Mazhar", "Uzma", "Fawad",
+                              "kalsoom", "amna", "Minal", "Uzair",
+                              "Faiza", "rehana", "naina", "Fakhira",
+                              "Ali", "rabeel", "Razia", "Aiza"};
+    int NameUsed[20] = {0};
 
-    for (int i = 0; i < numNames; ++i) {
-        strcpy(names[i], poolOfNames[i]);
+    struct Dept HR[5];
+    struct Dept Finance[5];
+    struct Dept Marketing[5];
+    struct Dept Logistics[5];
+
+    InitializeDept(HR, NamePool, NameUsed);
+    InitializeDept(Finance, NamePool, NameUsed);
+    InitializeDept(Marketing, NamePool, NameUsed);
+    InitializeDept(Logistics, NamePool, NameUsed);
+
+    int sum[4] = {DeptSum(HR), DeptSum(Finance), DeptSum(Marketing), DeptSum(Logistics)};
+    int maxIndex = 0;
+    int maxSum = sum[0];
+
+    for (int i = 1; i < 4; i++) {
+        if (sum[i] > maxSum) {
+            maxSum = sum[i];
+            maxIndex = i;
+        }
+    }
+
+    switch (maxIndex) {
+        case 0:
+            PrintDept(HR);
+            break;
+
+        case 1:
+            PrintDept(Finance);
+            break;
+
+        case 2:
+            PrintDept(Marketing);
+            break;
+
+        case 3:
+            PrintDept(Logistics);
+            break;
+
+        default:
+            break;
+    }
+
+    return 0;
+}
+
+char* NameRand(char Names[20][20], int Name_Used[20]) {
+    while (1) {
+        int randomIndex = rand() % 20;
+        if (!Name_Used[randomIndex]) {
+            Name_Used[randomIndex] = 1;
+            return Names[randomIndex];
+        }
     }
 }
 
-void initializeRoles(Department *dept, int numRoles) {
-    const char *poolOfRoles[] = {"Director", "Executive", "Manager", "Employee", "Trainee"};
-
-    for (int i = 0; i < numRoles; ++i) {
-        strcpy(dept[i].employees[i].role, poolOfRoles[i]);
+char* RoleRand(char Roles[5][20], int Role_Used[5]) {
+    while (1) {
+        int randomIndex = rand() % 5;
+        if (!Role_Used[randomIndex]) {
+            Role_Used[randomIndex] = 1;
+            return Roles[randomIndex];
+        }
     }
 }
 
-void initializeEmployees(Department *dept, char names[][MAX_NAME_LENGTH], int numEmployees) {
-    for (int i = 0; i < numEmployees; ++i) {
-        strcpy(dept->employees[i].name, names[i]);
-        dept->employees[i].communication = rand() % 100 + 1;
-        dept->employees[i].teamwork = rand() % 100 + 1;
-        dept->employees[i].creativity = rand() % 100 + 1;
+void InitializeDept(struct Dept dept[], char Names[20][20], int Name_Used[20]) {
+    char Roles[5][20] = {"Director", "Executive", "Manager ", "Employee", "Trainee "};
+    int RoleUsed[5] = {0};
+
+    for (int i = 0; i < 5; i++) {
+        strcpy(dept[i].Name, NameRand(Names, Name_Used));
+        strcpy(dept[i].Role, RoleRand(Roles, RoleUsed));
+        dept[i].Communication = rand() % 101;
+        dept[i].Creativity = rand() % 101;
+        dept[i].Teamwork = rand() % 101;
     }
 }
 
-void printDepartmentDetails(Department *dept) {
-    printf("| %-10s | %-10s | %-15s | %-8s | %-8s | %-10s |\n", "Department", "Name", "Role", "Communication", "Teamwork", "Creativity");
-    for (int i = 0; i < MAX_EMPLOYEES; ++i) {
-        printf("| %-10s | %-10s | %-15s | %-14d | %-8d | %-10d |\n", dept->name, dept->employees[i].name, dept->employees[i].role,
-               dept->employees[i].communication, dept->employees[i].teamwork, dept->employees[i].creativity);
+void PrintDept(struct Dept dept[]) {
+    printf("\n***************************************************************\n");
+    printf("| %-10s | %-15s | Communication | Creativity | Teamwork |\n", "Name", "Role");
+    printf("*************************************************************\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("| %-10s | %-15s | %-13d | %-10d | %-8d |\n",
+               dept[i].Name,
+               dept[i].Role,
+               dept[i].Communication,
+               dept[i].Creativity,
+               dept[i].Teamwork);
     }
-    printf("\n");
+
+    printf("*************************************************************\n");
 }
 
-int computeDepartmentSum(Department *dept) {
+int DeptSum(struct Dept dept[]) {
     int sum = 0;
-    for (int i = 0; i < MAX_EMPLOYEES; ++i) {
-        sum += dept->employees[i].communication + dept->employees[i].teamwork + dept->employees[i].creativity;
+    for (int i = 0; i < 5; i++) {
+        sum += dept[i].Communication + dept[i].Creativity + dept[i].Teamwork;
     }
     return sum;
 }
 
-int main() {
-    srand(time(NULL));
-
-    char names[MAX_EMPLOYEES * MAX_DEPARTMENTS][MAX_NAME_LENGTH];
-    initializeNames(names, MAX_EMPLOYEES * MAX_DEPARTMENTS);
-
-    Department departments[MAX_DEPARTMENTS] = {{"HR"}, {"Finance"}, {"Marketing"}, {"Logistics"}};
-
-    for (int i = 0; i < MAX_DEPARTMENTS; ++i) {
-        initializeRoles(departments + i, MAX_EMPLOYEES);
-        initializeEmployees(departments + i, names + i * MAX_EMPLOYEES, MAX_EMPLOYEES);
-    }
-
-    printf("Employee Details:\n");
-    for (int i = 0; i < MAX_DEPARTMENTS; ++i) {
-        printDepartmentDetails(departments + i);
-    }
-
-    int departmentSums[MAX_DEPARTMENTS];
-    for (int i = 0; i < MAX_DEPARTMENTS; ++i) {
-        departmentSums[i] = computeDepartmentSum(departments + i);
-    }
-
-    int bestDepartmentIndex = 0;
-    for (int i = 1; i < MAX_DEPARTMENTS; ++i) {
-        if (departmentSums[i] > departmentSums[bestDepartmentIndex]) {
-            bestDepartmentIndex = i;
-        }
-    }
-
-    printf("\nBest Department Award:\n");
-    printf("| %-15s | %-15s |\n", "Department", "Total Score");
-    for (int i = 0; i < MAX_DEPARTMENTS; ++i) {
-        printf("| %-15s | %-15d |\n", departments[i].name, departmentSums[i]);
-    }
-    printf("\nBest Department: %s\n", departments[bestDepartmentIndex].name);
-
-    return 0;
-}
